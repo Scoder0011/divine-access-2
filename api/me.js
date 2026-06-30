3c4f4c798e927fca23cd4a92857684645bee44d5
S_coder <scoder0011@gmail.com>
2026-07-01 04:15:11 +0530

    Update me.js

--- api/me.js
+++ api/me.js
@@ -1,1 +1,8 @@
+const { parseToken, getCookie } = require('./_lib/auth');
 
+module.exports = (req, res) => {
+  const token = getCookie(req, 'divine_token');
+  const user = token && parseToken(token);
+  if (!user) return res.status(401).json({ error: 'Not authenticated' });
+  return res.status(200).json({ username: user.username, role: user.role, hash: user.hash });
+};
